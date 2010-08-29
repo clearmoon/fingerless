26af1fc55206870e042c5444663cee7ca5736b6c
clearmoon <guangfengio@gmail.com>
2010-08-29 19:32:09 +0800

    fork my emacs setting repo

--- autosetting.sh
+++ autosetting.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#the .emacs is for myself,you can modify it from http://github.com/gfeng/joy-emacs.git
+#the .emacs is for myself,you can modify it from http://github.com/clearmoon/emacs.git
 #install
 echo "I will install emacs git ssh gcc unrar source-highlight and mysql-client\n";
 sudo apt-get install emacs git-core ssh build-essential unrar mysql-client source-highlight
 echo "install finished,I will set your emacs environment\n";
-git clone http://github.com/gfeng/joy-emacs.git
-cp joy-emacs/.emacs-ubuntu ~/.emacs && cp -r joy-emacs/site-lisp ~/site-lisp && rm -rf ~/joy-emacs
+git clone http://github.com/clearmoon/emacs.git
+cp emacs/.emacs-ubuntu ~/.emacs && cp -r emacs/site-lisp ~/site-lisp && rm -rf ~/emacs
 echo "set finished! Bye";
 
