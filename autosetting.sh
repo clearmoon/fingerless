37fcdfd0a271d8dc6f09c1d110e2af59c1feb25b
Guang Feng <gf@bot.(none)>
2010-08-29 17:47:41 +0800

    automatic install and set script for ubuntu

---
+++ autosetting.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#the .emacs is for myself,you can modify it from http://github.com/gfeng/joy-emacs.git
+#install
+echo "I will install emacs git ssh gcc unrar and mysql-client\n";
+sudo apt-get install emacs git-core ssh build-essential unrar mysql-client
+echo "install finished,I will set your emacs environment\n";
+git clone http://github.com/gfeng/joy-emacs.git
+cp joy-emacs/.emacs-ubuntu ~/.emacs && cp -r joy-emacs/site-lisp ~/site-lisp && rm -rf ~/joy-emacs
+echo "set finished! Bye";
+
