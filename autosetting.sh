19ce4c1b19ad4dad6bddc7637ca37040da7ed73f
clearmoon <gf@bot.(none)>
2010-08-29 18:02:34 +0800

    add source-highlight for muse source highlight

--- autosetting.sh
+++ autosetting.sh
@@ -2,7 +2,7 @@
 #the .emacs is for myself,you can modify it from http://github.com/gfeng/joy-emacs.git
 #install
 echo "I will install emacs git ssh gcc unrar and mysql-client\n";
-sudo apt-get install emacs git-core ssh build-essential unrar mysql-client
+sudo apt-get install emacs git-core ssh build-essential unrar mysql-client source-highlight
 echo "install finished,I will set your emacs environment\n";
 git clone http://github.com/gfeng/joy-emacs.git
 cp joy-emacs/.emacs-ubuntu ~/.emacs && cp -r joy-emacs/site-lisp ~/site-lisp && rm -rf ~/joy-emacs
